ad7771f5ea33d42b853675e415dbcfef12b75ba2
Alex Perry <alex.perry422@gmail.com>
2025-12-14 23:03:25 -0500

    unit 0 lesson 0

---
+++ alex-work/AkkaWordCounter/Program.cs
@@ -0,0 +1,22 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using AkkaWordCounter;
+
+ActorSystem myActorSystem = ActorSystem.Create("LocalSystem");
+myActorSystem.Log.Info("Hello from the ActorSystem");
+
+// Props == formula for create an actor
+Props myProps = Props.Create<HelloActor>();
+
+// IActorRef == handle for messaging an actor
+// Survives actor restarts, is serializable
+IActorRef myActor = myActorSystem.ActorOf(myProps, "MyActor");
+
+// tell my actor to display a message via fire-and-forget messaging
+myActor.Tell("Hello, World!");
+
+// use Ask<T> to do request-response messaging
+string whatsUp = await myActor.Ask<string>("What's up?");
+Console.WriteLine(whatsUp);
+
+await myActorSystem.Terminate();
---
+++ alex-work/AkkaWordCounter/README.md
@@ -0,0 +1,2 @@
+# Akka Tutorial
+following along with the following: https://petabridge.com/bootcamp/lessons/unit-0/first-akkadotnet-app/
